19c40d11bf5684a67684a0e6d58ec47a6ded3154
fishe108 <fishe108@ceclnx01-19.csi.miamioh.edu>
2022-05-04 19:04:33 -0400

    done

--- lispish.cs
+++ lispish.cs
@@ -1,29 +1,178 @@
+// Samuel Fisher (fishe108)
+// CSE 465
+// Project 2
+// Dr. Femiani
+// Due: April 30th, extended to May 3rd, 2022
+// Implementing a parser for a simple LISP-like language
+
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
-public class LispishParser
-{
-    public class Node
-    {
-        public void Print(string prefix = "")
-        {
+public class LispishParser {
+
+    public class Parser {
+        List<Node> tokens = new List<Node>();
+        int cur = 0;
+
+        public Parser(Node[] tokens) {
+            this.tokens = new List<Node>(tokens);
+            this.tokens.Add(new Node(Symbols.INVALID, ""));
+        }
+
+        public Node ParseProgram() {
+            var children = new List<Node>();
+            while (tokens[cur].Symbol != Symbols.INVALID) {
+                children.Add(ParseSExpr());
+            }
+            return new Node(Symbols.Program, children.ToArray());
+        }
+
+        public Node ParseSExpr() {
+            if (tokens[cur].Text == "(") {
+                return new Node(Symbols.SExpr, ParseList());
+            } else {
+                return new Node(Symbols.SExpr, ParseAtom());
+            }
+        }
+        
+	public Node ParseLiteral(string literal) {
+            if (tokens[cur].Text == literal) {
+                return tokens[cur++];
+            } else {
+                throw new Exception("Threw an exception on invalid input.");
+            }
+        }
+
+        public Node ParseList() {
+            Node node = tokens[cur];
+            cur++;
+            if (tokens[cur].Text == ")") {
+                return new Node(Symbols.List, node, tokens[cur]);
+            } else {
+                Node seq = ParseSeq();
+                if (tokens[cur].Text == ")") {
+                    return new Node(Symbols.List, node, seq, tokens[cur++]);
+                } else {
+                    throw new Exception ("Threw an exception on invalid input.");
+                }
+            }
+        }
+
+        public Node ParseSeq() {
+            Node sExp = (ParseSExpr());
+            if (tokens[cur].Text == ")") {
+                return new Node(Symbols.Seq, sExp);
+            } else {
+                return new Node(Symbols.Seq, sExp, ParseSeq());
+            }
+        }
+
+        public Node ParseAtom() {
+            if (tokens[cur].Symbol == Symbols.ID) {
+                return new Node(Symbols.Atom, tokens[cur++]);
+            } else if (tokens[cur].Symbol == Symbols.INT) {
+                return new Node(Symbols.Atom, tokens[cur++]);
+            } else if (tokens[cur].Symbol == Symbols.REAL) {
+                return new Node(Symbols.Atom, tokens[cur++]);
+            } else if (tokens[cur].Symbol == Symbols.STRING) {
+                return new Node(Symbols.Atom, tokens[cur++]);
+            } else {
+                throw new Exception("Threw an exception on invalid input.");
+            }
         }
     }
 
-    static public List<Node> Tokenize(String src)
-    {
-        return null;
+    // Holds all terminal and non-terminal symbols for the intended grammar
+    public enum Symbols {
+	Atom,
+        List,
+        Seq,
+        SExpr,
+        Program,
+	INVALID,
+	WS,
+	ID,
+	INT,
+	STRING,
+	LITERAL,
+	REAL
     }
 
-    static public Node Parse(Node[] tokens)
-    {
-        return null;
+    public class Node {
+        public Symbols Symbol;
+        public string Text = "";
+        List<Node> children = new List<Node>();
+
+        public Node(Symbols symbol, string text) {
+            this.Symbol = symbol;
+            this.Text = text;
+        }
+
+        public Node(Symbols symbol, params Node[] children) {
+            this.Symbol = symbol;
+            this.Text = "";
+            this.children = new List<Node>(children);
+        }
+
+	// Help with printing parse tree, written in Dr. Femiani's lecture
+        public void Print(string prefix = "") {
+            Console.WriteLine($"{prefix}{Symbol.ToString().PadRight(42 - prefix.Length)} {Text}");
+            foreach (var child in children) {
+                child.Print(prefix + "  ");
+            }
+        }
     }
 
-    static private void CheckString(string lispcode)
-    {
-        try
-        {
+    static public List<Node> Tokenize(String src) {
+        int pos = 0;
+        var result = new List<Node>();
+        Match mat;
+
+	// holding regular expression values for symbols
+        var WS = new Regex(@"\G\s");
+        var INT = new Regex(@"\G[+-]?[0-9]+");
+	var REAL = new Regex(@"\G\s*([+-]?[0-9]*\.[0-9]+)");
+	var STRING = new Regex(@"\G\s*""(\\.|[^""])*[^\\]""");
+	var ID = new Regex(@"\G\s*[^\s\""\\(\\)]+");
+	var LIT = new Regex(@"\G\s*([\\(\\)])");
+
+        while (pos < src.Length) {
+	    // Check if there is a match between input and
+	    // regular expression values written above
+	    // Correct order: WS, REAL, INT, STRING, ID, LIT
+            if ((mat = WS.Match(src, pos)).Success) {
+                pos += mat.Length;
+            } else if ((mat = REAL.Match(src, pos)).Success) {
+                result.Add(new Node(Symbols.REAL, mat.Value));
+                pos += mat.Length;
+            } else if ((mat = INT.Match(src, pos)).Success) {
+                result.Add(new Node(Symbols.INT, mat.Value));
+                pos += mat.Length;
+            } else if ((mat = STRING.Match(src, pos)).Success) {
+                result.Add(new Node(Symbols.STRING, mat.Value));
+                pos += mat.Length;
+            } else if ((mat = ID.Match(src, pos)).Success) {
+                result.Add(new Node(Symbols.ID, mat.Value));
+                pos += mat.Length;
+            } else if ((mat = LIT.Match(src, pos)).Success) {
+                result.Add(new Node(Symbols.LITERAL, mat.Value));
+                pos += mat.Length;
+            } else {
+                throw new Exception("Threw an exception on invalid input.");
+            }
+        }
+        return result;
+    }
+
+    static public Node Parse(Node[] tokens) {
+        var p = new Parser(tokens);
+        var tree = p.ParseProgram();
+        return tree;
+    }
+
+    static private void CheckString(string lispcode) {
+        try {
             Console.WriteLine(new String('=', 50));
             Console.Write("Input: ");
             Console.WriteLine(lispcode);
@@ -33,9 +182,8 @@ public class LispishParser
 
             Console.WriteLine("Tokens");
             Console.WriteLine(new String('-', 50));
-            foreach (Node node in tokens)
-            {
-                // TODO - print the node symbol and text
+            foreach (Node node in tokens) {
+                Console.WriteLine($"{node.Symbol,-21}\t: {node.Text}");
             }
             Console.WriteLine(new String('-', 50));
 
@@ -46,9 +194,8 @@ public class LispishParser
             parseTree.Print();
             Console.WriteLine(new String('-', 50));
         }
-        catch (Exception)
-        {
-            Console.WriteLine("Threw an exception on invalid input.");
+        catch (Exception e) {
+            Console.WriteLine(e.Message);
         }
     }
 
@@ -70,4 +217,3 @@ public class LispishParser
         CheckString(Console.In.ReadToEnd());
     }
 }
-
